a2e0e5829301537a90ea600130bd6fa61eb38b94
Tony <tony71318@gmail.com>
2017-05-01 12:43:46 +0800

    0501 test using php to use web3.js but failed

--- app/src/main/java/com/example/yuantabonus/yuantabonus/Setting.java
+++ app/src/main/java/com/example/yuantabonus/yuantabonus/Setting.java
@@ -5,13 +5,30 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class Setting extends AppCompatActivity {
 
+    OkHttpClient client = new OkHttpClient();
+
     Intent intent = new Intent();
+    TextView TextView_testPhp;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -53,6 +70,41 @@ public class Setting extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setSelectedItemId(R.id.navigation_settings);
+
+        TextView_testPhp = (TextView) findViewById(R.id.TextView_testPhp);
+        TextView_testPhp.setMovementMethod(new ScrollingMovementMethod());
+
+        testPhp(); //分割成獨立的function
+    }
+
+    public void testPhp(){
+
+        final ExecutorService service = Executors.newSingleThreadExecutor();
+
+        service.submit(new Runnable() {
+            @Override
+            public void run() {
+                try {
+
+                    Request request = new Request.Builder()
+                            .url("http://140.113.65.49/android/test.php")
+                            .build();
+
+                    final Response response = client.newCall(request).execute();
+                    final String resStr = response.body().string();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            TextView_testPhp.setText("test.php: " + resStr);
+                            Log.d("test.php: ", ""+resStr);
+                        }
+                    });
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
